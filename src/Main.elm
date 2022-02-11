27b20ef51cf8a260a5dffe4ffe120bf3007c1d07
Evan Mattiza <emattiza@gmail.com>
2022-02-11 03:22:16 +0000

    dirty state, need to get bin plotting in

--- src/Main.elm
+++ src/Main.elm
@@ -1,37 +1,197 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div)
-import Html.Attributes exposing (class)
+import Dict exposing (Dict)
+import Html exposing (Html, div, text)
+import Html.Attributes
+import List exposing (maximum, minimum)
+import Random exposing (Generator)
 import RemoteData exposing (RemoteData(..), WebData)
+import Svg exposing (Svg, g, line, rect, style, svg, text_)
+import Svg.Attributes exposing (fontSize, stroke, strokeDasharray, viewBox, x, x1, x2, y, y1, y2)
 
 
 type alias Model =
     { remoteNumbers : WebData (List Int) }
 
 
+randomIntList : Generator (List Int)
+randomIntList =
+    Random.list 200 (Random.int 1 10)
+
+
+getList : Cmd Msg
+getList =
+    Random.generate GotList randomIntList
+
+
 initialModel : Model
 initialModel =
-    { remoteNumbers = NotAsked }
+    { remoteNumbers = RemoteData.succeed [ 0 ] }
 
 
 type Msg
-    = Increment
+    = GotList (List Int)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model
+        GotList list ->
+            ( { model
+                | remoteNumbers =
+                    RemoteData.succeed list
+              }
             , Cmd.none
             )
 
 
 view : Model -> Html Msg
-view _ =
-    div [ class "btn-group" ]
-        []
+view model =
+    div [ Html.Attributes.class "container" ]
+        [ viewRandomList model.remoteNumbers ]
+
+
+viewRandomList : WebData (List Int) -> Html Msg
+viewRandomList data =
+    svg [ viewBox "0 0 1000 1000" ]
+        (case data of
+            Success list ->
+                viewGraph list
+
+            _ ->
+                []
+        )
+
+
+viewGraph : List Int -> List (Svg Msg)
+viewGraph nums =
+    let
+        xAxis =
+            line
+                [ x1 "5%"
+                , y1 "95%"
+                , x2 "5%"
+                , y2 "0%"
+                , stroke "gray"
+                ]
+                []
+
+        yAxis =
+            line
+                [ x1 "5%"
+                , y1 "95%"
+                , x2 "100%"
+                , y2 "95%"
+                , stroke "gray"
+                ]
+                []
+
+        yTicks tick ticks maxValue =
+            let
+                tickHeight =
+                    (95 / ticks * tick)
+                        |> round
+                        |> String.fromInt
+
+                tickValue =
+                    maxValue
+                        - (maxValue / ticks * tick)
+                        |> round
+                        |> String.fromInt
+
+                yTickLabel =
+                    text_
+                        [ x "2%"
+                        , y (tickHeight ++ "%")
+                        , fontSize "20px"
+                        , Svg.Attributes.class "tick-label"
+                        ]
+                        [ style
+                            []
+                            [ text ".tick-label {fill: white}"
+                            ]
+                        , text tickValue
+                        ]
+
+                yDashedLine =
+                    line
+                        [ stroke "gray"
+                        , strokeDasharray "3,3"
+                        , y1 <|
+                            tickHeight
+                                ++ "%"
+                        , y2 <|
+                            tickHeight
+                                ++ "%"
+                        , x1 "7%"
+                        , x2 "100%"
+                        ]
+                        []
+
+                yTickMark =
+                    line
+                        [ x1 "5%"
+                        , x2 "7%"
+                        , y1 <|
+                            tickHeight
+                                ++ "%"
+                        , y2 <|
+                            tickHeight
+                                ++ "%"
+                        , stroke "gray"
+                        ]
+                        []
+            in
+            g []
+                [ yTickMark
+                , yTickLabel
+                , yDashedLine
+                ]
+
+        viewBins : Dict Int Int -> List (Svg Msg)
+        viewBins binDict =
+            let
+                binKeys =
+                    Dict.keys binDict
+
+                minBin =
+                    minimum binKeys
+                        |> Maybe.withDefault 1
+
+                maxBin =
+                    maximum binKeys
+                        |> Maybe.withDefault 10
+            in
+            [ rect
+                []
+                []
+            ]
+
+        bins : Dict Int Int
+        bins =
+            List.foldl
+                (\k dict ->
+                    case Dict.get k dict of
+                        Just v ->
+                            Dict.insert k (v + 1) dict
+
+                        Nothing ->
+                            Dict.insert k 1 dict
+                )
+                Dict.empty
+                nums
+    in
+    List.append
+        [ xAxis
+        , yAxis
+        , yTicks 1 4 40
+        , yTicks 2 4 40
+        , yTicks 3 4 40
+        ]
+        (viewBins
+            bins
+        )
 
 
 subscriptions : Model -> Sub Msg
@@ -41,7 +201,7 @@ subscriptions _ =
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.none )
+    ( initialModel, getList )
 
 
 main : Program () Model Msg
